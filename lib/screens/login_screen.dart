67deec51618aa4dc88e71207e72a0c13de3a0d00
Langelihle2021 <langelihle2021@gmail.com>
2026-03-17 19:59:11 +0200

    Fix: Add missing flutter/gestures.dart import for TapGestureRecognizer in login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
 import 'package:provider/provider.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../services/auth_service.dart';
