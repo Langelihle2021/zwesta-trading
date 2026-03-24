b35b173a2b9c93cb21d26d7a172649ebb6cf3d40
Langelihle2021 <langelihle2021@gmail.com>
2026-03-24 23:11:00 +0200

    Update tunnel URL to eb1f287822e264.lhr.life

--- lib/utils/environment_config.dart
+++ lib/utils/environment_config.dart
@@ -4,13 +4,13 @@ enum Environment { development, staging, production }
 
 class EnvironmentConfig {
   // VPS Configuration (via SSH Tunnel - localhost.run)
-  static const String _vpsHost = '83c21ff52bf66e.lhr.life';
+  static const String _vpsHost = 'eb1f287822e264.lhr.life';
   static const int _vpsPort = 443; // HTTPS via SSH tunnel
 
   // Default API URLs - can be overridden by environment variables
-  static const String _devApiUrl = 'https://83c21ff52bf66e.lhr.life';
-  static const String _stagingApiUrl = 'https://83c21ff52bf66e.lhr.life';
-  static const String _prodApiUrl = 'https://83c21ff52bf66e.lhr.life';
+  static const String _devApiUrl = 'https://eb1f287822e264.lhr.life';
+  static const String _stagingApiUrl = 'https://eb1f287822e264.lhr.life';
+  static const String _prodApiUrl = 'https://eb1f287822e264.lhr.life';
 
   static const String _devApiKey = 'your_generated_api_key_here_dev';
   static const String _stagingApiKey = 'your_generated_api_key_here_staging';
